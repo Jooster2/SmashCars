4c611aaac8d8f52de76c34f222cc2769bdcdc083
Jooster2 <joakim.schmidt@gmail.com>
2016-02-11 14:40:58 +0100

    Newest CircularArray
    Signed-off-by: Jooster2 <joakim.schmidt@gmail.com>

--- controller/app/src/main/java/com/smashcars/CircularArray.java
+++ controller/app/src/main/java/com/smashcars/CircularArray.java
@@ -1,16 +1,16 @@
-package com.smashcars;
+package circularArray;
 import java.util.ArrayList;
 import java.util.List;
 /**
  * A Circular Array, where the element after the last is the first.
  * All methods should be capable of handling overflow
- *
- * Overflow in this context means that we have indexed more elements than the size of the
+ * 
+ * Overflow in this context means that we have added more elements than the size of the
  * underlying array, and therefore starts at the beginning again.
- *
+ * 
  * For example: A CircularArray with size 3. Add four elements. The fourth element will not fit
  * and thereby overflow onto the beginning of the array and overwrite the first element.
- *
+ * 
  * @author Joakim Schmidt
  * @version 1.0
  */
@@ -31,14 +31,14 @@ public class CircularArray<E> extends ArrayList<E> {
         readPtr = 0;
         initiateElements();
     }
-
+    
     /**
      * Constructor, will set the capacity to size. 
      * @param size the capacity of the list
      * @throws IllegalArgumentException if size is less than 1
      */
     public CircularArray(int size)
-            throws IllegalArgumentException {
+        throws IllegalArgumentException {
         super(size);
         if(size <= 0)
             throw new IllegalArgumentException("Size must be a positive integer");
@@ -49,13 +49,13 @@ public class CircularArray<E> extends ArrayList<E> {
             initiateElements();
         }
     }
-
+    
     /**
      * Ensures we have an initial capacity by adding null-elements
      */
     private void initiateElements() {
-        for(int i = 0; i < capacity; i++) {
-            super.add(null);
+    	for(int i = 0; i < capacity; i++) {
+        	super.add(null);
         }
     }
 
@@ -64,7 +64,7 @@ public class CircularArray<E> extends ArrayList<E> {
      * This method will overwrite any element found in the place it attempts to write to.
      */
     @Override
-    public boolean add(E e) {
+    public synchronized boolean add(E e) {
         super.set(writePtr, e);
         writePtr++;
         if(writePtr >= capacity)
@@ -72,31 +72,51 @@ public class CircularArray<E> extends ArrayList<E> {
         return true;
     }
 
+    /**
+     * This method should not be used in implementation. 
+     * It should only be used for testing purposes.
+     */
     @Override
     public void add(int index, E e) {
-        if(index > capacity)
-            index %= capacity;
+    	if(index > capacity)
+    		index %= capacity;
         //NOTE not sure this method should even do anything. If it should do something, then we need
         //NOTE to also be able to either remove the "last" element, or temporarily grow the array
 
+    }
+    
+    /**
+     * Overwrite the element at specified index, and returns the old element.
+     * @return the element previously at the specified index
+     */
+    @Override
+    public synchronized E set(int index, E e) {
+    	if(index > capacity)
+    		index %= capacity;
+    	return super.set(index, e);
     }
     /**
      * Removes and returns the next element in sequence.
      * @return the next element in sequence.
      */
-    public E getNext() {
-        E temp = remove(readPtr);
-        while(super.get(readPtr) == null) {
-            readPtr++;
-            if(readPtr >= capacity)
-                readPtr = 0;
-            //TODO on second thought the below if-statement is probably not correct
-            //There may actually be cases where the writePtr is in the way of readPtr
-            //Check and fix this
-            if(readPtr == writePtr)
-                break;
+    public synchronized E getNext() {
+    	int i = 0;
+    	E temp;
+        while((temp = remove(readPtr)) == null && i < capacity) {
+        	i++;
+        	moveReadPtr();
         }
+        moveReadPtr();
         return temp;
+    }
+    
+    /**
+     * Move the readPtr forward one step, and handle any overflow
+     */
+    private void moveReadPtr() {
+    	readPtr++;
+    	if(readPtr >= capacity)
+    		readPtr = 0;
     }
 
     /**
@@ -106,9 +126,9 @@ public class CircularArray<E> extends ArrayList<E> {
      * @return a list containing all the elements found between fromIndex and toIndex in the correct sequence.
      */
     @Override
-    public List<E> subList(int fromIndex, int toIndex) {
-        if(toIndex > capacity)
-            toIndex %= capacity;
+    public synchronized List<E> subList(int fromIndex, int toIndex) {
+    	if(toIndex > capacity) 
+    		toIndex %= capacity;
         if(fromIndex <= toIndex)
             return super.subList(fromIndex, toIndex);
         else {
@@ -123,9 +143,9 @@ public class CircularArray<E> extends ArrayList<E> {
      * @return the element found at the specified index
      */
     @Override
-    public E remove(int index) {
-        if(index > capacity)
-            index %= capacity;
+    public synchronized E remove(int index) {
+    	if(index > capacity)
+    		index %= capacity;
         return super.set(index, null);
     }
 
@@ -134,8 +154,8 @@ public class CircularArray<E> extends ArrayList<E> {
      * @return true if this list contains no elements
      */
     @Override
-    public boolean isEmpty() {
-        //TODO this method could use a better (simpler) check
+    public synchronized boolean isEmpty() {
+    	//TODO this method could use a better (simpler) check
         for(E element : super.subList(0, capacity)) {
             if(element != null)
                 return false;
