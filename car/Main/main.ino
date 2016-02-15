05507f2adfbccc763ae5fa8652e2d1b4bbc6c4fc
DJdavjohns <davjohns@student.chalmers.se>
2016-02-15 13:40:13 +0100

    Added code for RF receiver. Car will now forward any RF message via bluetooth to phone

--- car/Main/main.ino
+++ car/Main/main.ino
@@ -2,9 +2,11 @@
 #define BACKWARD 1
 #define SERVO 2
 #define BRAKE 3
+#define RF_RECEIVE_PIN 11
+
 #include <Servo.h>
 #include <SoftwareSerial.h>  
-Servo servo;
+#include <VirtualWire.h>
 
 boolean driveForward;
 boolean driveBackwards;
@@ -16,13 +18,15 @@ int motorDir = 13;
 int motor = 11;
 int motorBrakePin = 8;
 int servoPin = 7;
+Servo servo;
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
-
 
  
 void setup() {
- Serial.begin(9600);
- bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
+  Serial.begin(9600);// Debugging only
+  Serial.println("setup");
+ 
+  bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
   bluetooth.print("$");  // Print three times individually
   bluetooth.print("$");
   bluetooth.print("$");  // Enter command mode
@@ -33,107 +37,96 @@ void setup() {
   pinMode (motorDir, OUTPUT);
   pinMode (motor, OUTPUT);
   pinMode (motorBrakePin, OUTPUT);
-
+  
   servo.attach (servoPin);
   servo.write (90);
-
+  
   driveForward = false;
   driveBackwards = false;
   servoTurn = false;
   motorBrake= true;
-  
+
+  // Initialise RF receiver)
+  vw_set_rx_pin(RF_RECEIVE_PIN);
+  vw_setup(2000);  // Bits per sec
+  vw_rx_start();   // Start the receiver PLL running
 }
 
-void loop() {
+void loop() {  
+  int cmd;
+  int val;
   
-int cmd;
-int val;
-  
- if( bluetooth.available() ){       // if data is available to read
-  cmd = readData();
-  Serial.println("COMMANDS : ");
-  Serial.println(cmd);
-  val = readData();
-  Serial.println("VALUES : ");
-  Serial.println(val);
-  demask(cmd);
-  if(servoTurn)               
-    {  
+  if( bluetooth.available() ){       // if data is available to read
+    cmd = readData();
+    Serial.println("COMMANDS : ");
+    Serial.println(cmd);
+    val = readData();
+    Serial.println("VALUES : ");
+    Serial.println(val);
+    demask(cmd);
+    if(servoTurn){  
      turnServo(val);
-    }
-    else if (driveForward){
-       motorDrive(FORWARD,val);
-      }
-  
-     else if (driveBackwards)
-     {
+    }else if (driveForward){
+      motorDrive(FORWARD,val);
+    }else if (driveBackwards){
       motorDrive(BACKWARD ,val);
-     }
-     else if (motorBrake)
-     {
-      
-     }
-      delay (50);
+    }else if (motorBrake){
+    }
+    delay (50);
   }
   else {
-    
-  }
-}
-int readData(){
-    int data = (int)bluetooth.read();
-    return data;
   }
 
-void motorDrive(int dir, int val)
-{
+  //RF stuff
+  uint8_t buf[1];
+  uint8_t buflen = 1;
+  if (vw_get_message(buf, &buflen))
+    bluetooth.write(buf[0]);
+}
+
+int readData(){
+  int data = (int)bluetooth.read();
+  return data;
+}
+
+void motorDrive(int dir, int val){
   if (dir = 0){
     digitalWrite (motorDir, HIGH); 
   }
   else {
-  digitalWrite (motorDir, LOW);
+    digitalWrite (motorDir, LOW);
   }
   digitalWrite (motorBrake, LOW);
   analogWrite(motor, val);
-  
 }
 
-void brake()
-{
+void brake(){
   digitalWrite (motorBrakePin, HIGH);
 }
-void turnServo(int val)
-{
-    servo.write (val); 
-    Serial.println("Servo vrid");
-    Serial.println(val);
+void turnServo(int val){
+  servo.write (val); 
+  Serial.println("Servo vrid");
+  Serial.println(val);
 }
 
-
 void demask(int cmd){
-
   int temp = cmd & 0b00000111;
   if (temp == 1){
-    
     driveForward =true;
     driveBackwards = false;
     motorBrake = false;
     servoTurn = false;
-  }
-  else if (temp == 3){
+  }else if (temp == 3){
     driveForward =false;
     driveBackwards = true;
     motorBrake = false;
     servoTurn = false;
-  }
-  else if (temp == 0)
-  {
+  }else if (temp == 0){
     driveForward =false;
     driveBackwards = false;
     motorBrake = false;
     servoTurn = true;
-  }
-  else if (temp = 4)
-  {
+  }else if (temp = 4){
     driveForward =false;
     driveBackwards = false;
     motorBrake = true;
